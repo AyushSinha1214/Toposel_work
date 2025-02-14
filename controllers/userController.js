075f976a31b391b300d4c9c28a46bd1c8d2c5403
AyushSinha1214 <ayushsinha1214@gmail.com>
2025-02-14 14:31:58 +0530

    Initial commit - Toposel Backend

---
+++ controllers/userController.js
@@ -0,0 +1,53 @@
+const User = require('../models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+exports.registerUser = async (req, res) => {
+  try {
+    const { username, email, password, fullName, gender, dob, country } = req.body;
+    
+    let user = await User.findOne({ email });
+    if (user) return res.status(400).json({ message: 'User already exists' });
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+    
+    user = new User({ username, email, password: hashedPassword, fullName, gender, dob, country });
+    await user.save();
+
+    res.status(201).json({ message: 'User registered successfully' });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: 'Invalid Credentials' });
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return res.status(400).json({ message: 'Invalid Credentials' });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+exports.getUser = async (req, res) => {
+  try {
+    const { username, email } = req.query;
+    let user = null;
+
+    if (username) user = await User.findOne({ username }).select('-password');
+    else if (email) user = await User.findOne({ email }).select('-password');
+
+    if (!user) return res.status(404).json({ message: 'User not found' });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
